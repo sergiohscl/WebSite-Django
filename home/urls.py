bd25b7743fc021957c624a5ecd344c1c5487920b
sergiohscl <sergiohscl2@gmail.com>
2025-04-18 12:46:32 -0300

    :sparkeles: ajuste css

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from django_distill import distill_path
 from . import views
 
 
+# função que retorna parâmetros de URL (aqui só uma rota sem parâmetro)
+def get_home():
+    yield {}
+
+
 urlpatterns = [
+    # rota normal (útil pra debug local)
     path('', views.home, name='home'),
+    # rota para distill
+    distill_path('', views.home, name='home',
+                 distill_func=get_home,
+                 distill_file='index.html'),
 ]
